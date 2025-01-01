994e0a8e3991593628af71b4e5372061fbb800fe
Jonathan Guaynora <guaynorajon@gmail.com>
2025-01-01 15:58:26 -0500

    added initial setup for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,28 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { TitheModule } from './tithe/tithe.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+const env = process.env;
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<MercuriusDriverConfig>({
       driver: MercuriusDriver,
       autoSchemaFile: true,
       graphiql: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: Number(env.DB_PORT),
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      database: env.DATABASE,
+      synchronize: true,
+      autoLoadEntities: true,
     }),
     TitheModule,
   ],
--- src/tithe/domain/entities/tithe.entity.ts
+++ src/tithe/domain/entities/tithe.entity.ts
@@ -1,13 +1,18 @@
-import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { ObjectType, Field, ID, Float } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'tithe' })
 @ObjectType()
 export class Tithe {
+  @PrimaryGeneratedColumn('uuid')
   @Field(() => ID, { description: 'id of tithe' })
   id: string;
 
-  @Field(() => Int, { description: 'amount of tithe' })
+  @Column({ type: 'float' })
+  @Field(() => Float, { description: 'amount of tithe' })
   amount: number;
 
+  @Column({ type: 'date' })
   @Field(() => Date, { description: 'date of tithe' })
   date: Date;
 }
