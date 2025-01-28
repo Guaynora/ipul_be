8c934e94e86159e1c5cec03f46e2f2279f01bae8
Jonathan Guaynora <guaynorajon@gmail.com>
2025-01-27 23:22:16 -0500

    added error on create tithe

--- src/tithe/application/services/tithe.service.ts
+++ src/tithe/application/services/tithe.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Tithe } from 'src/tithe/domain';
 import { Repository } from 'typeorm';
@@ -6,20 +10,28 @@ import { CreateTitheInput, UpdateTitheInput } from '../dto';
 
 @Injectable()
 export class TitheService {
+  private readonly logger = new Logger(TitheService.name);
+
   constructor(
     @InjectRepository(Tithe)
     private readonly titheRepository: Repository<Tithe>,
   ) {}
 
   async create(createTitheInput: CreateTitheInput): Promise<Tithe> {
-    console.log('create tithe =>', createTitheInput);
-    return await this.titheRepository.save(createTitheInput);
+    try {
+      const tithe = this.titheRepository.create(createTitheInput);
+      return await this.titheRepository.save(tithe);
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
   async findAll(): Promise<Tithe[]> {
-    const tithes = await this.titheRepository.find();
-    console.log('tithes =>', tithes);
-    return tithes;
+    try {
+      return await this.titheRepository.find();
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
   async findOne(id: string): Promise<Tithe> {
@@ -32,5 +44,13 @@ export class TitheService {
 
   async remove(id: string) {
     return `This action removes a #${id} tithe`;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505') {
+      throw new InternalServerErrorException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unespected error');
   }
 }
